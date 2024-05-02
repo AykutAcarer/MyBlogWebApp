cdd8374fbedb8b50bc10f44bb70becf8727a608d
AykutAcarer <acareraykut@gmail.com>
2024-05-02 22:55:06 +0200

    created configuration of database

--- app/views/home.php
+++ app/views/home.php
@@ -1,1 +1,11 @@
-This is the home page
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $data['page_title'] ?></title>
+</head>
+<body>
+    <h1>This is Home Page</h1>
+</body>
+</html>
