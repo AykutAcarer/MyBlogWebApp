5de34bca31625016e44a37c5e286d364baebc040
AykutAcarer <acareraykut@gmail.com>
2024-05-18 20:28:39 +0200

    config updated

--- app/core/config.php
+++ app/core/config.php
@@ -13,7 +13,7 @@ define('DB_PASS','');
 define('DB_HOST','localhost');
 
 /*protocal type http or https*/
-define('PROTOCAL','http');
+define('PROTOCAL','https');
 
 /*root and asset paths*/
 
@@ -21,7 +21,7 @@ $path = str_replace("\\", "/",PROTOCAL ."://" . $_SERVER['SERVER_NAME'] . __DIR_
 $path = str_replace($_SERVER['DOCUMENT_ROOT'], "", $path);
 
 define('ROOT', str_replace("app/core", "public", $path));
-define('ASSETS', str_replace("app/core", "public", $path));
+define('ASSETS', str_replace("app/core", "public/assets", $path));
 
 /*set to true to allow error reporting
 set to false when you upload online to stop error reporting*/
