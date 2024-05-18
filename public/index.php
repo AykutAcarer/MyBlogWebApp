dee45efcb5bc77bcd7e1a74a806fad8d4364bf3f
AykutAcarer <acareraykut@gmail.com>
2024-05-18 20:09:35 +0200

    config updated

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php 
 session_start();                    #requires for logging in and out
 
-require "../app/init.php";          #include init.php file because -init- file include every other important file 
+require "app/init.php";          #include init.php file because -init- file include every other important file 
     
 $app = new App();                   #Create an Instance from App class to get function in App class
 
